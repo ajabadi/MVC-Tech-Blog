3a2656ae53bc363eda4b3a7df4fb813f3a58ec8f
ajabadi <ajabadi@ucdavis.edu>
2024-01-04 15:32:39 -0800

    Update production database configuration to PostgreSQL

--- config/config.js
+++ config/config.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   "development": {
+    // Your development configuration
     "username": process.env.DB_USER,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_NAME,
@@ -9,6 +10,7 @@ module.exports = {
     "dialect": "mysql"
   },
   "test": {
+    // Your test configuration
     "username": process.env.DB_USER,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_NAME,
@@ -16,12 +18,12 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "use_env_variable": "JAWSDB_URL",
-    "dialect": "mysql",
+    "use_env_variable": "DATABASE_URL",
+    "dialect": "postgres",
     "dialectOptions": {
       "ssl": {
         "require": true,
-        "rejectUnauthorized": false // For Heroku deployment
+        "rejectUnauthorized": false // For Heroku deployment with PostgreSQL
       }
     }
   }
